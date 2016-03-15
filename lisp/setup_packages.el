4592650f6009bd617a110088a07bf9283d45ec3b
hitswint <wgq_hit@126.com>
2016-03-15 17:34:39 +0800

    Added functions to diff regions.

--- lisp/setup_packages.el
+++ lisp/setup_packages.el
@@ -646,12 +646,24 @@ is named like ODF with the extension turned to pdf."
   ;; pyim-delete-word-from-personal-buffer
   ;; 关闭使用双拼。
   ;; (setq pyim-default-pinyin-scheme 'pyim-shuangpin)
+  (defun swint-pyim-probe-dynamic-english ()
+    "Changed the behaviour of chinese-pyim at beginning of line."
+    (if (or (= (- (point) (point-at-bol)) (current-indentation))
+            (= (point) (point-at-bol)))
+        (not (or (pyim-string-match-p "\\cc" (save-excursion
+                                               ;; 查找前一个非空格字符。
+                                               (if (re-search-backward "[^[:space:]]" nil t)
+                                                   (char-to-string (char-after (point))))))
+                 (> (length pyim-current-key) 0)))
+      (pyim-probe-dynamic-english)))
   (setq-default pyim-english-input-switch-functions
                 '(pyim-probe-org-speed-commands
                   pyim-probe-org-structure-template
                   pyim-probe-isearch-mode
                   pyim-probe-program-mode
-                  pyim-probe-dynamic-english))
+                  ;; 在minibuffer关闭中文输入。
+                  window-minibuffer-p
+                  swint-pyim-probe-dynamic-english))
   (setq-default pyim-punctuation-half-width-functions
                 '(pyim-probe-punctuation-line-beginning
                   pyim-probe-punctuation-after-punctuation))
