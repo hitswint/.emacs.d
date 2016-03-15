dbf84a8d846d0b7e32739b6f3723509ee1085042
hitswint <wgq_hit@126.com>
2016-03-15 17:34:39 +0800

    Added function to diff regions.

--- defuns/editing-defuns.el
+++ defuns/editing-defuns.el
@@ -234,7 +234,7 @@ if point is at end of line , new-line-and-indent"
     (progn
       (open-line 1)
       (indent-for-tab-command)))
-   ((= (- (point) (point-at-bol))(current-indentation))
+   ((= (- (point) (point-at-bol)) (current-indentation))
     (progn
       (split-line)
       (indent-for-tab-command)))
@@ -268,3 +268,88 @@ if point is at end of line , new-line-and-indent"
     (dotimes (i 10)
       (when (= p (point)) ad-do-it))))
 ;; =================jump to mark==================
+;; ===============diff two regions==================
+;; Step 1: Select a region and `M-x diff-region-tag-selected-as-a'
+;; Step 2: Select another region and `M-x diff-region-compare-with-b'
+(defvar swint-diff-region-tag nil
+  "Stores the current list of buffers that will be searched through.")
+(defun diff-region-format-region-boundary (b e)
+  "Make sure lines are selected and B is less than E"
+  (let (tmp rlt)
+    ;; swap b e, make sure b < e
+    (when (> b e)
+      (setq tmp b)
+      (setq b e)
+      (set e tmp))
+    ;; select lines
+    (save-excursion
+      ;; Another workaround for evil-visual-line bug:
+      ;; In evil-mode, if we use hotkey V or `M-x evil-visual-line` to select line,
+      ;; the (line-beginning-position) of the line which is after the last selected
+      ;; line is always (region-end)! Don't know why.
+      (if (and (> e b)
+               (save-excursion (goto-char e) (= e (line-beginning-position)))
+               (boundp 'evil-state) (eq evil-state 'visual))
+          (setq e (1- e)))
+      (goto-char b)
+      (setq b (line-beginning-position))
+      (goto-char e)
+      (setq e (line-end-position)))
+    (setq rlt (list b e))
+    rlt))
+(defun diff-region-tag-selected-as-a ()
+  "Select a region to compare"
+  (interactive)
+  (setq swint-diff-region-tag t)
+  (when (region-active-p)
+    (let (tmp buf)
+      ;; select lines
+      (setq tmp (diff-region-format-region-boundary (region-beginning) (region-end)))
+      (setq buf (get-buffer-create "*Diff-regionA*"))
+      (save-current-buffer
+        (set-buffer buf)
+        (erase-buffer))
+      (append-to-buffer buf (car tmp) (cadr tmp))))
+  (message "Now select other region to compare and run `diff-region-compare-with-b`"))
+(defun diff-region-compare-with-b ()
+  "Compare current region with region selected by `diff-region-tag-selected-as-a' "
+  (interactive)
+  (setq swint-diff-region-tag nil)
+  (if (region-active-p)
+      (let (rlt-buf
+            diff-output
+            (fa (make-temp-file (expand-file-name "scor"
+                                                  (or small-temporary-file-directory
+                                                      temporary-file-directory))))
+            (fb (make-temp-file (expand-file-name "scor"
+                                                  (or small-temporary-file-directory
+                                                      temporary-file-directory)))))
+        (when fb
+          (setq tmp (diff-region-format-region-boundary (region-beginning) (region-end)))
+          (write-region (car tmp) (cadr tmp) fb))
+        (setq rlt-buf (get-buffer-create "*Diff-region-output*"))
+        (when (and fa (file-exists-p fa) fb (file-exists-p fb))
+          (save-current-buffer
+            (set-buffer (get-buffer-create "*Diff-regionA*"))
+            (write-region (point-min) (point-max) fa))
+          (setq diff-output (shell-command-to-string (format "diff -Nabur %s %s" fa fb)))
+          ;; show the diff output
+          (if (string= diff-output "")
+              (message "Two regions are SAME!")
+            (save-current-buffer
+              (switch-to-buffer-other-window rlt-buf)
+              (set-buffer rlt-buf)
+              (erase-buffer)
+              (insert diff-output)
+              (diff-mode))))
+        (if (and fa (file-exists-p fa))
+            (delete-file fa))
+        (if (and fb (file-exists-p fb))
+            (delete-file fb)))
+    (message "Please select region at first!")))
+(global-set-key (kbd "M-s =") '(lambda ()
+                                 (interactive)
+                                 (if swint-diff-region-tag
+                                     (diff-region-compare-with-b)
+                                   (diff-region-tag-selected-as-a))))
+;; ===============diff two regions==================
