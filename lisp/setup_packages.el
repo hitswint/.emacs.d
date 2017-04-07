62377a273aacbd987e4cbb79f7385185a57536fd
hitswint <wgq_hit@126.com>
2017-04-07 09:13:47 +0800

    Fixed markdown-command config.

--- lisp/setup_packages.el
+++ lisp/setup_packages.el
@@ -928,7 +928,6 @@ is named like ODF with the extension turned to pdf."
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+         ("\\.markdown\\'" . markdown-mode)))
 ;; =================markdown-mode==================
 (provide 'setup_packages)
