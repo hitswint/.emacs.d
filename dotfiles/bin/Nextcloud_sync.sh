2a0f30e168b90919752c46eb3dc71930fb5ba962
hitswint <wgq_hit@126.com>
2023-05-14 16:17:24 +0800

    Changed bin name.

--- dotfiles/bin/Nextcloud_sync.sh
+++ dotfiles/bin/Nextcloud_sync.sh
@@ -6,4 +6,4 @@ user=$(gpg2 -q --for-your-eyes-only --no-tty -d ~/.authinfo.gpg | awk '$2==serve
 pass=$(gpg2 -q --for-your-eyes-only --no-tty -d ~/.authinfo.gpg | awk '$2==server {print $NF}' server="$server")
 port=$(gpg2 -q --for-your-eyes-only --no-tty -d ~/.authinfo.gpg | awk '$2==server {print $4}' server="$server")
 
-java -Dderby.system.home=$HOME/.webdav_sync/ -Dbe.re.http.no-compress -jar $HOME/.webdav_sync/webdav_sync1_1_6.jar -r -bi -u http://$user:$pass@$host:$port/remote.php/dav/files/swint/Downloads/ -d $HOME/Downloads/
+java -Dderby.system.home=$HOME/.webdav_sync/ -Dbe.re.http.no-compress -jar $HOME/.webdav_sync/webdav_sync1_1_6.jar -r -bi -u https://$user:$pass@$host:$port/nextcloud/remote.php/dav/files/swint/Downloads/ -d $HOME/Downloads/
--- dotfiles/bin/cad-2008.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-Filename="${1//\//\\}"
-env LANG=zh_CN.UTF-8 wine ~/.wine/drive_c/Program\ Files/AutoCAD\ 2008/acad.exe "$Filename"
