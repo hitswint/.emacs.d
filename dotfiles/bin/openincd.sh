d204b92915aebb0822ec5d3275e1ea93655d11f7
hitswint <wgq_hit@126.com>
2024-03-17 21:26:48 +0800

    Improvements.

--- dotfiles/bin/openincd.sh
+++ dotfiles/bin/openincd.sh
@@ -3,16 +3,26 @@
 Wind_id=`xdotool getactivewindow`;
 sleep 0.2
 
-if [ $(xdotool search --onlyvisible --class "Emacs" | grep -i $Wind_id) ]; then
+curr_dir="`xcwd`"
+
+if [[ $(xdotool search --onlyvisible --class "Emacs" | grep -i $Wind_id) ]]; then
     curr_dir=$(emacsclient --eval "(openincd-directory)")
-    eval $1 $curr_dir
-elif [ $(xdotool search --onlyvisible --class "qpdfview" | grep -i $Wind_id) ]; then
+elif [[ $(xdotool search --onlyvisible --class "qpdfview" | grep -i $Wind_id) ]]; then
     config_file=$HOME/.config/qpdfview/qpdfview.conf
     data_file=$HOME/.local/share/qpdfview/qpdfview/database
     curr_tab=$(cat $config_file | grep currentTabIndex | sed 's/currentTabIndex=//g')
     curr_file=$(sqlite3 $data_file "select filePath from tabs_v5" | awk 'NR==curr_tab' curr_tab=`expr $curr_tab + 1`)
     curr_dir=$(dirname -- "$curr_file")
-    eval $1 $curr_dir
-else
-    eval $1 "`xcwd`"
+elif [[ $(xdotool search --onlyvisible --class "Wps|Et|Wpp|Wpspdf|Wpsoffice" | grep -i $Wind_id) || $curr_dir == $HOME ]]; then
+    PID=$(xdotool getactivewindow getwindowpid)
+    curr_file=$(ls /proc/$PID/fd/* | xargs -L 1 readlink | grep -E "$HOME|/mnt" | grep -vE "$HOME/(.local|.config)" | grep -v "$HOME/\.\+.*" | rofi.sh -no-custom -auto-select -dmenu -i -p "Open:")
+    if [[ $? -eq 1 ]]; then     # C-g取消时返回1，否则返回0
+        exit
+    fi
+    if [[ $curr_file ]]; then
+        curr_dir=$(dirname -- "$curr_file")
+    fi
 fi
+
+# eval $1 "`xcwd`"
+eval $1 $curr_dir
--- dotfiles/bin/rofi-open.sh
+++ dotfiles/bin/rofi-open.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 win_id=`xdotool getactivewindow`;
-sleep 0.5
+sleep 0.2
 
-if [ $(xdotool search --onlyvisible --class "Emacs" | grep -i $win_id) ]; then
+if [[ $(xdotool search --onlyvisible --class "Emacs" | grep -i $win_id) ]]; then
     curr_file=$(emacsclient --eval "(openincd-file)" | tr -d '"')
-elif [ $(xdotool search --onlyvisible --class "qpdfview" | grep -i $win_id) ]; then
+elif [[ $(xdotool search --onlyvisible --class "qpdfview" | grep -i $win_id) ]]; then
     config_file=$HOME/.config/qpdfview/qpdfview.conf
     data_file=$HOME/.local/share/qpdfview/qpdfview/database
     curr_tab=$(cat $config_file | grep currentTabIndex | sed 's/currentTabIndex=//g')
@@ -14,14 +14,18 @@ else
     # PID=$(xprop -id `xdotool getwindowfocus` | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$')
     PID=$(xdotool getactivewindow getwindowpid)
     # curr_file=$(lsof -u $USER -a -b -p $PID | grep REG | grep -E "$HOME|/mnt" | grep -vE "$HOME/(.local|.config)")
-    curr_file=$(ls /proc/$PID/fd/* | xargs -L 1 readlink | grep -E "$HOME|/mnt" | grep -vE "$HOME/(.local|.config)" | rofi.sh -auto-select -dmenu -i -p "Open:" | tr -d '\r\n')
+    curr_file=$(ls /proc/$PID/fd/* | xargs -L 1 readlink | grep -E "$HOME|/mnt" | grep -vE "$HOME/(.local|.config)" | grep -v "$HOME/\.\+.*" | rofi.sh -no-custom -auto-select -dmenu -i -p "Open:")
+    if [[ $? -eq 1 ]]; then     # C-g取消时返回1，否则返回0
+        exit
+    fi
+fi
+
+if [[ $1 == "other" ]]; then
+    [[ $curr_file ]] && curr_dir=$(dirname -- "$curr_file") || curr_dir=$HOME
+    curr_file=$(rofi.sh -modi filebrowser -show filebrowser -filebrowser-directory "$curr_dir" -run-command "echo {cmd}" | sed 's/xdg-open //g')
 fi
 
 if [[ $curr_file ]]; then
-    if [[ $1 == "other" ]]; then
-        curr_dir=$(dirname -- "$curr_file")
-        curr_file=$(rofi.sh -modi filebrowser -show filebrowser -filebrowser-directory "$curr_dir" -run-command "echo {cmd}" | sed 's/xdg-open //g')
-    fi
     select_command=$(rofi.sh -show run -run-command "echo {cmd}")
     if [[ $select_command ]]; then
         eval $select_command \"$curr_file\"
