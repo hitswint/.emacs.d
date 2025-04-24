22e78947a823d346f0b7056f0ed70d4628823194
hitswint <wgq_hit@126.com>
2025-04-24 21:04:38 +0800

    Improved setup for eaf.

--- lisp/setup_eaf.el
+++ lisp/setup_eaf.el
@@ -239,22 +239,25 @@
     (eaf-bind-key eaf-pdf-narrow-search "/" eaf-pdf-viewer-keybinding)
     (defvar pdf-opened-file-path (concat eaf-config-location "pdf/history/log_opened.txt"))
     (advice-add 'eaf--monitor-emacs-kill :before #'(lambda () (ignore-errors
-                                                                (let ((pdf-urls ""))
-                                                                  (write-region
-                                                                   (dolist (buffer (eaf--get-eaf-buffers) pdf-urls)
-                                                                     (with-current-buffer buffer
-                                                                       (when (and (equal eaf--buffer-app-name "pdf-viewer")
-                                                                                  (not (funcall projectile-ignored-project-function eaf--buffer-url)))
-                                                                         (setq pdf-urls (concat eaf--buffer-url "\n" pdf-urls)))))
-                                                                   nil pdf-opened-file-path)))))
-    (when (file-exists-p pdf-opened-file-path)
-      (let ((pdf-opened-files (with-temp-buffer (insert-file-contents pdf-opened-file-path)
-                                                (split-string (buffer-string) "\n" t))))
-        (cl-loop for pdf in pdf-opened-files
-                 when (file-exists-p pdf)
-                 do (progn
-                      (sit-for 0.2)
-                      (eaf-open pdf))))))
+                                                                (let* ((pdf-urls "")
+                                                                       (pdf-opened-files (dolist (buffer (eaf--get-eaf-buffers) pdf-urls)
+                                                                                           (with-current-buffer buffer
+                                                                                             (when (and (equal eaf--buffer-app-name "pdf-viewer")
+                                                                                                        (not (funcall projectile-ignored-project-function eaf--buffer-url)))
+                                                                                               (setq pdf-urls (concat eaf--buffer-url "\n" pdf-urls)))))))
+                                                                  (unless (string-empty-p pdf-opened-files)
+                                                                    (write-region pdf-opened-files nil pdf-opened-file-path))))))
+    (advice-add 'eaf-open-pdf-from-history :around #'(lambda (fn) (if (file-exists-p pdf-opened-file-path)
+                                                                      (progn (let ((pdf-opened-files (with-temp-buffer (insert-file-contents pdf-opened-file-path)
+                                                                                                                       (split-string (buffer-string) "\n" t))))
+                                                                               (cl-loop for pdf in pdf-opened-files
+                                                                                        when (file-exists-p pdf)
+                                                                                        do (progn
+                                                                                             (sit-for 0.2)
+                                                                                             (eaf-open pdf))))
+                                                                             (delete-file pdf-opened-file-path))
+                                                                    (eaf-pdf-delete-invalid-file-record-from-history)
+                                                                    (funcall fn)))))
   (use-package eaf-image-viewer
     :config
     (eaf-bind-key load_prev_image "S-SPC" eaf-image-viewer-keybinding)
