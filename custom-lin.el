bc29cf192f8800f88ed324d7376df8378ddce784
hitswint <wgq_hit@126.com>
2026-03-09 12:10:43 +0800

    Added edit-indirect.

--- custom-lin.el
+++ custom-lin.el
@@ -20,35 +20,36 @@
                        dired-duplicates dired-filetype-face dired-narrow
                        dired-ranger dired-subtree disable-mouse disaster
                        djvu dogears drag-stuff dumb-jump easy-kill ebib
-                       elisp-slime-nav ellama elmacro elpy emmet-mode
-                       emms eshell-prompt-extras evil-nerd-commenter
-                       exec-path-from-shell expand-region fcitx
-                       find-file-in-project flycheck-pos-tip
-                       function-args gcmh git-timemachine
-                       gnu-elpa-keyring-update gnuplot-mode god-mode
-                       google-translate goto-chg graphviz-dot-mode
-                       helm-ag helm-bibtex helm-bm helm-descbinds
-                       helm-firefox helm-flycheck helm-flyspell
-                       helm-gtags helm-mu helm-pass helm-projectile
-                       helm-swoop helm-unicode highlight-parentheses
-                       highlight-symbol hungry-delete imenu-anywhere
-                       imenu-list inheritenv ivy-bibtex ivy-hydra jedi
-                       jupyter key-chord lacarte lingva magit
-                       markdown-mode matlab-mode meghanada modelica-mode
-                       msgpack mu4e-alert mu4e-views multifiles
-                       multiple-cursors names neotree nov
-                       operate-on-number org-appear org-brain org-noter
-                       org-pdftools outline-magic outshine ox-pandoc
-                       paredit-everywhere pass pdf-tools pdfgrep
-                       peep-dired pinyin-search posframe pyim
-                       pyim-basedict quickrun rainbow-delimiters
-                       rainbow-mode readline-complete recentf-ext
-                       restclient rg skewer-mode smartrep sudo-edit
-                       tangotango-theme term-keys unicode-escape vcard
-                       vimish-fold visible-mark visual-regexp vlf
-                       volatile-highlights vterm vundo w3m web-mode
-                       window-numbering wrap-region yaml-mode
-                       yasnippet-snippets youdao-dictionary ztree)))
+                       edit-indirect elisp-slime-nav ellama elmacro elpy
+                       emmet-mode emms eshell-prompt-extras
+                       evil-nerd-commenter exec-path-from-shell
+                       expand-region fcitx find-file-in-project
+                       flycheck-pos-tip function-args gcmh
+                       git-timemachine gnu-elpa-keyring-update
+                       gnuplot-mode god-mode google-translate goto-chg
+                       graphviz-dot-mode helm-ag helm-bibtex helm-bm
+                       helm-descbinds helm-firefox helm-flycheck
+                       helm-flyspell helm-gtags helm-mu helm-pass
+                       helm-projectile helm-swoop helm-unicode
+                       highlight-parentheses highlight-symbol
+                       hungry-delete imenu-anywhere imenu-list
+                       inheritenv ivy-bibtex ivy-hydra jedi jupyter
+                       key-chord lacarte lingva magit markdown-mode
+                       matlab-mode meghanada modelica-mode msgpack
+                       mu4e-alert mu4e-views multifiles multiple-cursors
+                       names neotree nov operate-on-number org-appear
+                       org-brain org-noter org-pdftools outline-magic
+                       outshine ox-pandoc paredit-everywhere pass
+                       pdf-tools pdfgrep peep-dired pinyin-search
+                       posframe pyim pyim-basedict quickrun
+                       rainbow-delimiters rainbow-mode readline-complete
+                       recentf-ext restclient rg skewer-mode smartrep
+                       sudo-edit tangotango-theme term-keys
+                       unicode-escape vcard vimish-fold visible-mark
+                       visual-regexp vlf volatile-highlights vterm vundo
+                       w3m web-mode window-numbering wrap-region
+                       yaml-mode yasnippet-snippets youdao-dictionary
+                       ztree)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
