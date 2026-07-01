ed3a3d6081c0bacd651c2105877ec51e9474a59c
hitswint <wgq_hit@126.com>
2026-07-02 07:06:18 +0800

    Fixes.

--- snippets/LaTeX-mode/.yas-setup.el
+++ snippets/LaTeX-mode/.yas-setup.el
@@ -1,9 +1,9 @@
 (require 'yasnippet)
 
 (defvar org-beamer-available-themes (mapcar #'(lambda (x) (substring-no-properties (file-name-sans-extension x) 11 nil))
-                                            (directory-files "/usr/share/texlive/texmf-dist/tex/latex/beamer" nil "^beamertheme.+\\.sty")))
+                                            (ignore-errors (directory-files "/usr/share/texlive/texmf-dist/tex/latex/beamer" nil "^beamertheme.+\\.sty"))))
 (defvar org-beamer-available-colorthemes (mapcar #'(lambda (x) (substring-no-properties (file-name-sans-extension x) 16 nil))
-                                                 (directory-files "/usr/share/texlive/texmf-dist/tex/latex/beamer" nil "^beamercolortheme.+\\.sty")))
+                                                 (ignore-errors (directory-files "/usr/share/texlive/texmf-dist/tex/latex/beamer" nil "^beamercolortheme.+\\.sty"))))
 
 (defun tikz-second-arg (type)
   (cond
